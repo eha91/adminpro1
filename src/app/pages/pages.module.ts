531e24a3ad09cfdf93bd18e0ed57abb849c575fe
Eduardo Hdz <eha.developer@gmail.com>
2018-02-04 13:39:53 -0600

    Agregamos y Modificamos Moduloes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { ProgressComponent } from '../pages/progress/progress.component';
+import { Graficas1Component } from '../pages/graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+import  {PAGES_ROUTES} from '../pages/pages.routes';
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { NopagefoundComponent } from '../shared/nopagefound/nopagefound.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from '../shared/breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+    declarations: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ],
+    exports: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ]
+})
+export class SharedModule {}
